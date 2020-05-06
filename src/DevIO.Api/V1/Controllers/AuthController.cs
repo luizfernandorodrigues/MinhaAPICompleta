dc607b7072f7d1ae712f8fc2f106ceb6c3a4a287
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-05-06 19:20:09 -0300

    Implementado versionamento da APi

--- src/DevIO.Api/V1/Controllers/AuthController.cs
+++ src/DevIO.Api/V1/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using DevIO.Api.Extensions;
+﻿using DevIO.Api.Controllers;
+using DevIO.Api.Extensions;
 using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
 using Microsoft.AspNetCore.Identity;
@@ -12,9 +13,10 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DevIO.Api.Controllers
+namespace DevIO.Api.V1.Controllers
 {
-    [Route("api")]
+    [ApiVersion("1.0"]
+    [Route("api/v{version:apiVersion}")]
     public class AuthController : MainController
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -114,8 +116,8 @@ namespace DevIO.Api.Controllers
                 {
                     Id = user.Id,
                     Email = user.Email,
-                    Claims = claims.Select(c=> new ClaimViewModel
-                    { 
+                    Claims = claims.Select(c => new ClaimViewModel
+                    {
                         Type = c.Type,
                         Value = c.Value
                     })
